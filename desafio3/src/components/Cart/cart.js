55364b914649acdf2267076c40dd7c9662612542
Eddy <edgar.33.1993@gmail.com>
2021-04-15 21:58:59 -0300

    cambios desafio clase 11

---
+++ desafio3/src/components/Cart/cart.js
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { Link } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
+import '../styles/cart.css'
+
+export const Cart = () => {
+
+    const {cart,removeItem,totalItems,totalPrecio,clear} = useContext(CartContext)
+
+
+
+    return (
+        <div>
+            {
+             !cart.length ?   
+               <h2>No hay elementos en el carrito <Link class="bttn-gradient bttn-md bttn-primary" to='/' >Ir al home </Link> </h2>
+              :  (<>
+                {cart.map(cartItem => (
+                <div className="Productos" key={cartItem.item.id} >
+                    <div className=""> Titulo:  {cartItem.item.title}  </div>
+                    <div> cantidad: {cartItem.quantity} </div>
+                    <button className="bttn-gradient bttn-md bttn-success" onClick={()=> removeItem(cartItem.item.id)}>borrar</button>
+                </div>)
+
+                
+                )}
+                <div>Total:{totalItems} y {totalPrecio}</div> 
+                <button className="bttn-gradient bttn-md bttn-success" onClick={clear}>Borrar todo</button>         
+                </>
+            )
+            }
+
+        </div>
+    )
+}
---
+++ desafio3/src/components/firebase/index.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const app = firebase.initializeApp(
+    {
+        apiKey: "AIzaSyBMD0yLKlnzzvQRo6ENWgjCtO2r6tnwXzg",
+        authDomain: "ecommerse-react.firebaseapp.com",
+        projectId: "ecommerse-react",
+        storageBucket: "ecommerse-react.appspot.com",
+        messagingSenderId: "970180699819",
+        appId: "1:970180699819:web:3a76b07c2ce14f2e4479ee"
+      }
+)
+
+
+    
