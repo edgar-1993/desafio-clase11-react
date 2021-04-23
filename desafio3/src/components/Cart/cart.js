7c7274ba1d6532289ab9a444b496a310c686797c
Eddy <edgar.33.1993@gmail.com>
2021-04-22 23:48:37 -0300

    arme formulario firabase

--- desafio3/src/components/Cart/cart.js
+++ desafio3/src/components/Cart/cart.js
@@ -8,61 +8,75 @@ import {getFirestore} from '../firebase/index'
 
 export const Cart = () => {
 
+    const [name,setName] = useState('')
+    const [phone,setPhone] = useState('')
+    const [email,setEmail] = useState('')
+
+    const [idOrden, setIdOrden ] = useState(null)
+
     const {cart,removeItem,totalItems,totalPrecio,clear} = useContext(CartContext)
 
-    const generarOrden = () =>{
+
+    const finalizarCompra = (e) =>{
+        generarOrden(e)
+        clear()
+    }
+
+    const generarOrden = (e)=>{
+        e.preventDefault();
+        const comprador = { name, phone, email }
+        
+        console.log(comprador)
+
         const db = getFirestore();
 
-        const ordersCol = db.collection('orders');
+        const ordersCollection = db.collection("orders")
 
-        let orden = {}
-        orden.date = firebase.firestore.Timestamp.fromDate(new Date());
+        const date = firebase.firestore.Timestamp.fromDate(new Date());
 
-        orden.buyer = { name:'Edgar', phone:'1134470765', email:'edgar.espinoladavid@gmail.com'}
-        orden.total = totalPrecio;
-        orden.items = cart.map(cartItem => {
-            const id = cartItem.item.id;
-            const title = cartItem.item.title;
-            const price = cartItem.item.price * cartItem.quantity;
+        const items = cart.map(cartItem => {
+            return {id: cartItem.item.id, title:cartItem.item.title, price: cartItem.item.price} 
+        })
 
-            return {id, title, price}   
+        ordersCollection
+        .add({buyer: comprador, items , date, total:totalPrecio })
+        .then(doc=>{
+            setIdOrden(doc.id)
+        });
+
+
+        let itemIds = cart.map((e) => e.item.id)
+
+        const itemsCollection = db.collection("items")
+        .where(firebase.firestore.FieldPath.documentId(), "in", itemIds)
+
+        itemsCollection.get()
+        .then(resultado =>{
+            const batch = db.batch()
+
+            for (const documento of resultado.docs) {
+
+                const stockActual = documento.data().stock;
+
+                const itemDelCart = cart.find(cartItem => cartItem.item.id == documento.id);
+
+                const cantidadComprado = itemDelCart.quantity;
+
+                const nuevoStock =  stockActual - cantidadComprado;
+
+
+                batch.update(documento.ref,
+                    {stock: nuevoStock}
+                )
+                //update
+                
+            }
+
+            batch.commit()
+
         })
 
 
-        ordersCol.add(orden)
-        
-        .then((IdDocumento)=>{
-            console.log(IdDocumento.id)
-        })
-        .catch( err => {
-            console.log(err);
-        })
-        .finally(()=>{
-            console.log('termino la promesa')
-        })
-
-        const itemsToUpdate = db.collection('items').where(
-            firebase.firestore.FieldPath.documentId(), 'in', cart.map(i=> i.item.id)
-        )
-
-        const batch = db.batch();
-
-        // por cada item restar del stock la cantidad de el carrito
-
-        itemsToUpdate.get()
-        .then( collection=>{
-            collection.docs.forEach(docSnapshot => {
-                batch.update(docSnapshot.ref,{
-                    stock: docSnapshot.data().stock - cart.find(item => item.item.id === docSnapshot.id).quantity
-                })
-            })
-
-            batch.commit().then(res =>{
-                console.log('resultado batch:', res)
-            })
-        })
-
-        console.log(orden)
     }
    
 
@@ -71,11 +85,15 @@ export const Cart = () => {
        
        <div>
          
-        
+         {idOrden? `Orden generada : Numero----> ${idOrden}`: null}
+
             {
              !cart.length ?   
                <h2>No hay elementos en el carrito <Link class="bttn-gradient bttn-md bttn-primary" to='/' >Ir al home </Link> </h2>
               :  (<>
+
+             
+             
                 {cart.map(cartItem => (
                 <div className="Productos" key={cartItem.item.id} >
                     <div className=""> Titulo:  {cartItem.item.title}  </div>
@@ -90,9 +108,15 @@ export const Cart = () => {
                 <button className="bttn-gradient bttn-md bttn-danger" onClick={clear}>Borrar todo</button>
                
             
-                <Link class="bttn-gradient bttn-md bttn-primary" to='/' onClick={generarOrden}>Finalizar compra </Link>  
+                  
                
-               
+                <form className="" action=""  onSubmit={finalizarCompra}>
+                    <input className="formulario mb-5" type="text" value={name} onChange={(e)=>setName(e.target.value)}/>Nombre
+                    <input className="formulario mb-5" type="text" value={phone} onChange={(e)=>setPhone(e.target.value)}/>Telefono
+                    <input className="formulario mb-5" type="text" value={email} onChange={(e)=>setEmail(e.target.value)}/>Email
+                    <button type="submit"> Generar orden</button>
+                </form> 
+
                 </>
             )
             }
@@ -100,3 +124,32 @@ export const Cart = () => {
         </div>
     )
 } 
+
+
+
+
+
+
+
+
+
+/*buyer
+(mapa)
+email
+"edgar.espinoladavid@gmail.com"
+name
+"Edgar"
+phone
+"1134470765"
+date
+21 de abril de 2021 a las 21:38:30 UTC-3
+items
+0
+id
+"2CLD9nzf4gIOe4djn9pJ"
+price
+4750
+title
+"Hamburguesa vegano 1"
+total
+4750 */
